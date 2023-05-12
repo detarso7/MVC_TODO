b35fa13e2a47ed6f75608325afc28c2b82f496f5
Saulo de Tarso <de_tarso7@hotmail.com>
2023-05-12 15:40:52 -0300

    Inserindo e Removendo Tasks

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -6,8 +6,37 @@ module.exports = class TaskController {
         res.render('tasks/create')
     }
 
-    static showTask (req, res) {
-        res.render('tasks/all')
+    static async createTaskSave (req, res) {
+
+        const tasks = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
+
+        //Validações
+
+        //Processar Dados
+
+        await Task.create(tasks)
+
+        res.redirect('/tasks')
+    }
+
+    static async removeTask (req, res) {
+
+        const id = req.body.id
+
+        await Task.destroy({where: {id: id}})
+
+        res.redirect('/tasks')
+    }
+
+    static async showTask (req, res) {
+
+       const tasks = await Task.findAll({raw: true})
+
+        res.render('tasks/all', {tasks})
     }
 
 }
